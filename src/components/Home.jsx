0682ae8cbca6cbfbe98dbc2c3b272c10e6d7e7b6
sall <keinpark1@naver.com>
2024-10-29 00:40:10 +0900

    feat : navbar 구현

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+// src/pages/Home.jsx
+import React from "react";
+// import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <p>Home페이지</p>
+    </div>
+  );
+};
+
+export default Home;

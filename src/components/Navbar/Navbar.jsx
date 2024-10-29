c082ac9cc999153f2e34e18a7ffaeaf4c307ca8d
sall <keinpark1@naver.com>
2024-10-30 00:21:32 +0900

    fix : navbar 크기 수정,인기 블로그,개선 피드백 제거

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,16 +21,8 @@ const Navbar = () => {
           사용 기술
         </li>
         <li>
-          {/* <Link to="/blog">인기 블로그</Link> */}
-          인기 블로그
-        </li>
-        <li>
           {/* <Link to="/criteria">검사 기준</Link> */}
           검사 기준
-        </li>
-        <li>
-          {/* <Link to="/feedback">개선 피드백</Link> */}
-          개선 피드백
         </li>
         <li>{/* <Link to="/profile">내 정보</Link> */}내 정보</li>
       </ul>

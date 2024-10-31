12e4689fbc4ab0a10d429bc7fe312a6f9bac01a9
nuyeo <bluelayzxx@gmail.com>
2024-10-31 11:44:41 +0900

    KAN-58 feat: 임시 결과 페이지 추가
    결과 페이지 리디렉션을 위한 샘플 페이지 추가

---
+++ src/components/Result/Result.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Result = () => {
+    return (
+        <div>
+            <h1>Result Page</h1>
+            <p>This is the result page.</p>
+        </div>
+    );
+};
+
+export default Result;

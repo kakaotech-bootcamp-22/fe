868daa8db636f9b2b9553a3f492124682fb5cb67
dayaelee <dayae.dev@gmail.com>
2024-11-16 04:34:07 +0900

    KAN-54 feat: AuthContext에서 로그인 상태, 토큰, 닉네임, 프로필 이미지 관리 추가

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,17 +1,35 @@
-import React, { createContext, useContext, useState } from "react";
+import { createContext, useState, useContext, useEffect } from 'react';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [token, setToken] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(null);
+  const [nickname, setNickname] = useState(null); // 닉네임 상태 추가
+  const [profileImage, setProfileImage] = useState(null); // 프로필 이미지 상태 추가
 
-    const login = (newToken) => setToken(newToken);
-    const logout = () => setToken(null);
-    return (
-        <AuthContext.Provider value={{ token, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  // 로그인 메서드
+  const login = (newToken, newNickname, newProfileImage) => {
+    setToken(newToken);
+    setNickname(newNickname); // 닉네임 저장
+    setProfileImage(newProfileImage); // 프로필 이미지 저장
+    setIsLoggedIn(true);
+  };
+
+  // 로그아웃 메서드
+  const logout = () => {
+    setToken(null);
+    setNickname(null);
+    setProfileImage(null);
+    setIsLoggedIn(false);
+  };
+
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, token, nickname, profileImage, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => useContext(AuthContext);

68d17c81c6316f8be59a1df0531dd978cd368369
dayaelee <dayae.dev@gmail.com>
2024-11-18 03:40:23 +0900

    KAN-54 feat: 로그인 페이지에서 Google 로그인 처리 로직 추가

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-
-import loginImage from "../../assets/login/babyCat.png"
-import kakaologinImage from "../../assets/login/kakao_login_large_narrow.png"
-import googleloginImage from "../../assets/login/google_logo.png"
+import React, { useEffect, useState, useRef } from "react";
+import loginImage from "../../assets/login/babyCat.png";
+import kakaologinImage from "../../assets/login/kakao_login_large_narrow.png";
+import googleloginImage from "../../assets/login/google_logo.png";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { useAuth } from "../../context/AuthContext";
 import axios from 'axios';
+import { useGoogleLogin } from '@react-oauth/google';
+import Cookies from "js-cookie";
+import { useNavigate } from 'react-router-dom';
 
 import {
     Container,
@@ -27,12 +28,16 @@ function LoginPage(props) {
     const [isKakaoInitialized, setIsKakaoInitialized] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
 
+    const navigate = useNavigate();
+
     useEffect(() => {
         // Kakao SDK 초기화
         const { Kakao } = window;
+        const kakaoJsKey = process.env.REACT_APP_KAKAO_JS_KEY;
+
         if (Kakao) {
             if (!Kakao.isInitialized()) {
-                Kakao.init("826a723547312cf55037f1bf217f293b");
+                Kakao.init(kakaoJsKey);
                 setIsKakaoInitialized(true); // 초기화 성공 시 상태 업데이트
             }
         } else {
@@ -46,12 +51,37 @@ function LoginPage(props) {
             setErrorMessage("Kakao SDK가 로드되지 않았거나 초기화되지 않았습니다.");
             return;
         }
-
         Kakao.Auth.authorize({
             redirectUri: "http://localhost:3000/", // 리다이렉트할 페이지
         });
 
     };
+
+    const GoogleLoginButton = () => {
+        const signIn = useGoogleLogin({
+            onSuccess: (res) => {
+                axios.post('http://localhost:8080/auth/google/token', {
+                    access_token: res.access_token,
+                })
+                    .then(response => {
+                        Cookies.set('accessToken', response.data.token);
+                        navigate('/');
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            },
+            onError: (error) => { console.log(error); }
+        });
+        return (
+            <div onClick={() => signIn()}>
+                <GoogleloginButton>
+                    <GoogleImage src={googleloginImage} />
+                    <GoogleLoginText>구글 계정으로 로그인</GoogleLoginText>
+                </GoogleloginButton>
+            </div>
+        );
+    }
 
     return (
         <Container>
@@ -64,16 +94,15 @@ function LoginPage(props) {
                     </TextBox>
                     <KakaologinButton onClick={handleKakaoLogin}>
                         <img src={kakaologinImage} />
-                    </KakaologinButton>
-                    <GoogleloginButton>
-                        <GoogleImage src={googleloginImage} />
-                        <GoogleLoginText> 구글 계정으로 로그인 </GoogleLoginText>
-                    </GoogleloginButton>
+                    </KakaologinButton>         
+                    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+                        <GoogleLoginButton />
+                    </GoogleOAuthProvider>
                 </ContainerVertical>
             </Background>
         </Container>
     );
-
 }
+
 
 export default LoginPage;

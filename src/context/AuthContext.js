752c50924d78ca0c96ec25f017e97819d117cd50
dayaelee <dayae.dev@gmail.com>
2024-11-21 02:05:22 +0900

    KAN-55 feat: 로그인 상태 확인시 백엔드에서 받아온 이메일 주소 추가 반영

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -9,16 +9,17 @@ export const AuthProvider = ({ children }) => {
   const [profileImage, setProfileImage] = useState(null); // 프로필 이미지 상태 추가
   const [platform, setPlatform] = useState(null); // 플랫폼 추가
   const [createdAt, setCreatedAt] = useState(null); // 플랫폼 추가
+  const [email, setEmail] = useState(null);
 
   // 로그인 메서드
-  const login = (newToken, newNickname, newProfileImage, newPlatform, newCreatedAt) => {
+  const login = (newToken, newNickname, newProfileImage, newPlatform, newCreatedAt, newEmail) => {
     setToken(newToken);
     setNickname(newNickname); // 닉네임 저장
     setProfileImage(newProfileImage); // 프로필 이미지 저장
     setPlatform(newPlatform) // 플랫폼 저장
     setCreatedAt(newCreatedAt)
     setIsLoggedIn(true);
-
+    setEmail(newEmail)
   };
 
   // 로그아웃 메서드
@@ -28,16 +29,19 @@ export const AuthProvider = ({ children }) => {
     setProfileImage(null);
     setPlatform(null);
     setIsLoggedIn(false);
-    setCreatedAt(null)
-    // console.log("로그아웃 완료");
+    setCreatedAt(null);
+    setEmail(null);
+  };
+
+  const updateProfileImage = (newImage) => {
+    setProfileImage(newImage);
   };
 
   useEffect(() => {
-   });
-
+  });
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, token, nickname, profileImage, platform, createdAt, login, logout }}>
+    <AuthContext.Provider value={{ isLoggedIn, token, nickname, profileImage, platform, createdAt, email, login, logout, updateProfileImage }}>
       {children}
     </AuthContext.Provider>
   );

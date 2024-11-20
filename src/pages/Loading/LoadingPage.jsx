eb16d6c44be6c077771e9342d1d363d5fc137442
nuyeo <bluelayzxx@gmail.com>
2024-11-20 16:21:11 +0900

    KAN-59 feat: 로딩페이지에 5초 간격으로 폴링 로직 추가

---
+++ src/pages/Loading/LoadingPage.jsx
@@ -0,0 +1,60 @@
+// src/components/Loading.jsx
+import React, { useEffect, useState } from 'react';
+import './Loading.css';
+import chunshikImage from '../../assets/loading/loading_image.webp'; // 춘식이 이미지
+
+const LoadingPage = () => {
+  const [activeDot, setActiveDot] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const dotInterval = setInterval(() => {
+      setActiveDot((prevDot) => (prevDot + 1) % 3); // 0, 1, 2 반복
+    }, 500); // 0.5초마다 변경
+
+    return () => clearInterval(dotInterval);
+  }, []);
+
+  useEffect(() => {
+    const pollingInterval = 5000; //  5초 간격으로 폴링
+    let intervalId;
+
+    const checkResultStatus = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/result'); // 백엔드 API URL
+        const result = await response.json();
+
+        if (result.status === 'READY') {
+          navigate('/result', { state: result.data });
+        }
+      } catch (err) {
+        console.error('데이터를 가져오는 데 실패했습니다.', err);
+      }
+    };
+
+    // 폴링 시작
+    intervalId = setInterval(checkResultStatus, pollingInterval);
+    checkResultStatus(); // 첫 번째 요청 즉시 호출
+
+    // 컴포넌트가 언마운트 되면 폴링 중지
+    return () => clearInterval(intervalId);
+  }, [requestId, navigate]);
+
+  return (
+    <div className="loading-container">
+      <img src={chunshikImage} alt="Loading character" className="loading-image" />
+      <p>Loading...</p>
+      <div className="dots">
+        {[0, 1, 2].map((dot) => (
+          <span
+            key={dot}
+            className={`dot ${dot === activeDot ? 'active' : ''}`}
+          ></span>
+        ))}
+      </div>
+      <p className="loading-text">URL을 AI 모델이 분석하고 있어요!</p>
+    </div>
+  );
+};
+
+export default LoadingPage;

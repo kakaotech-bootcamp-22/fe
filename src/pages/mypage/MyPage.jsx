6d4047b66428eca165a78b2888075e9bb6831b9a
dayaelee <dayae.dev@gmail.com>
2024-12-11 18:39:25 +0900

    KAN-155 style: 마이페이지 회원 탈퇴 버튼 위치 조 정

--- src/pages/mypage/MyPage.jsx
+++ src/pages/mypage/MyPage.jsx
@@ -22,7 +22,7 @@ const EntireContainer = styled.div`
 const ProfileContainer = styled.div`
   max-width: 500px;
   margin: 0 auto;
-  padding: 20px;
+  padding: 10px;
   background-color: #ffffff;
   text-align: center;
   min-height: 90vh;
